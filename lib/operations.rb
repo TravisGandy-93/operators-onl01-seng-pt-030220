03655e2702400b21bb0e7f241943090003c36161
Travis Gandy <travis_gandy@yahoo.com>
2020-03-08 05:12:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed < 40
+    "true"
+  elsif speed > 60
+   "true"
+  else
+    "false"
 end
-
+end
 
 
 def not_safe?(speed)
-	
+	speed < 60 ? "false" : "true"
+	speed > 40 ? "false" : "true"
 end
 	
-
 
